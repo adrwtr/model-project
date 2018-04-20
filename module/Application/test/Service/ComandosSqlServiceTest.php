e73c0413d170ff777f7319a405f6548fb4dfc757
Adriano Waltrick <adriano@unimestre.com>
2018-04-20 17:26:25 -0300

    WIP - trabalho em progresso

--- module/Application/test/Service/ComandosSqlServiceTest.php
+++ module/Application/test/Service/ComandosSqlServiceTest.php
@@ -63,9 +63,6 @@ class ComandosSqlServiceTest  extends TestCase
         );
     }
 
-
-
-
     public function testForeingKeyParse()
     {
         $ds_sql = "
@@ -103,4 +100,61 @@ class ComandosSqlServiceTest  extends TestCase
             $arrTabelas[0]['arrForeingkey']
         );
     }
+
+    public function testForeingKeyDuplo() {
+        $ds_sql = "
+        CREATE TABLE `unim_moodle_cursos` (
+          `cd_moodle_curso` int(10) unsigned NOT NULL AUTO_INCREMENT,
+          `ds_descricao` varchar(255) DEFAULT NULL,
+          `ds_sigla` varchar(255) DEFAULT NULL,
+          `dt_revisao` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+          `cd_integracao_externa` smallint(3) DEFAULT NULL,
+          PRIMARY KEY (`cd_moodle_curso`),
+          UNIQUE KEY `uk_sigla` (`ds_sigla`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+        CREATE TABLE `unim_moodle_cursos_disciplinas` (
+          `cd_curso_disciplina` int(10) unsigned NOT NULL AUTO_INCREMENT,
+          `cd_moodle_curso` int(10) unsigned NOT NULL,
+          `nr_anosemestre` smallint(6) unsigned NOT NULL,
+          `cd_curso` varchar(15) NOT NULL,
+          `cd_turma` varchar(50) NOT NULL,
+          `id_disciplina` int(11) unsigned NOT NULL,
+          PRIMARY KEY (`cd_curso_disciplina`),
+          KEY `unim_mcdf1` (`cd_moodle_curso`),
+          KEY `unim_mcdf2` (`nr_anosemestre`,`cd_turma`,`cd_curso`),
+          KEY `unim_mcdf3` (`id_disciplina`),
+          CONSTRAINT `ufk_mcdf1` FOREIGN KEY (`cd_moodle_curso`) REFERENCES `unim_moodle_cursos` (`cd_moodle_curso`) ON DELETE CASCADE ON UPDATE CASCADE
+        ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+        ";
+
+        $objComandosSqlService = new ComandosSqlService(
+            new PHPSQLParser()
+        );
+
+        $objComandosSqlService->parse($ds_sql);
+        $arrTabelas = $objComandosSqlService->getArrTabelas();
+
+        $this->assertEquals(
+            5,
+            count($arrTabelas[0]['arrCampos'])
+        );
+
+        $this->assertEquals(
+            0,
+            count($arrTabelas[0]['arrForeingkey'])
+        );
+
+        $this->assertEquals(
+            6,
+            count($arrTabelas[1]['arrCampos'])
+        );
+
+        $this->assertEquals(
+            1,
+            count($arrTabelas[0]['arrForeingkey'])
+        );
+
+
+    }
 }
+
