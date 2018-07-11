b5acb07917f20ec301a344f930964b9aa37c4c03
Adriano Waltrick <adriano@unimestre.com>
2018-07-11 08:08:47 -0300

    WIP - FK insert

--- module/Application/src/Service/Dql/TabelaChaveDqlService.php
+++ module/Application/src/Service/Dql/TabelaChaveDqlService.php
@@ -6,29 +6,30 @@ class TabelaChaveDqlService
     public function getCamposChaveFromTabela() {
         return '
             select
-              tc
+                tc.id as id,
+                
+                tdc.id as tabela_origem_id,
+                tdc.ds_nome as ds_nome_chave,
+                
+                td.id as tabela_destino_id,
+                td.ds_nome as ds_nome_tabela_destino,
+                
+                co.id as campo_origem_id,
+                co.ds_nome as ds_nome_campo_origem,
+                
+                cd.id as campo_destino_id,
+                cd.ds_nome as ds_nome_campo_destino
             from 
                 \Application\Entity\TabelaChave tc
+                
+                inner join tc.objTipoDeChave tdc
+                inner join tc.objTabelaDestino td
+                inner join tc.objCampoOrigem co
+                inner join tc.objCampoDestino cd
+                
             where
                 tc.objTabelaOrigem = :tabela_origem_id
             ';
-
-        // -- ,
-        //
-        //tc.id as id,
-        //                tc.tabela_origem_id as tabela_origem_id,
-        //                tc.tabela_destino_id as tabela_destino_id,
-        //                tc.campo_origem_id as campo_origem_id,
-        //                tc.campo_destino_id as campo_destino_id
-        //
-        //                tp.ds_nome as ds_nome_chave
-/*
-                inner join \Application\Entity\TipoDeChave tp ON (
-                    tp.id = tc.tipo_de_chave_id
-                )
-            where
-                tc.tabela_origem_id = :tabela_origem_id
-        ';*/
     }
 }
 
