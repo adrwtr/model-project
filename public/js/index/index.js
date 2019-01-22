db09e07194ff23ed9ba0fbb7f44ed4f5c3a7dcbd
Adriano Waltrick <adriano@unimestre.com>
2019-01-22 08:04:50 -0200

    Refatoracao pagina index - layout

---
+++ public/js/index/index.js
@@ -0,0 +1,58 @@
+var app_lista_tabela = new Vue({
+    el: '#vueApp-lista-tabela',
+
+    data: {
+        arrSistemas: [],
+        arrSistemasOriginal : [],
+        sn_carregando : true,
+        ds_filtro : '',
+    },
+
+    created: function() {
+        this.getListRegistros();
+    },
+
+    watch: {
+        ds_filtro: function ()
+        {
+            var ds_filtrar = this.ds_filtro;
+
+            this.arrSistemas = this.arrSistemasOriginal;
+
+            if (_.isEmpty(ds_filtrar)) {
+                return;
+            }
+
+            this.arrSistemas = _.filter(
+                this.arrSistemasOriginal,
+                function (arrValor) {
+                    return arrValor.ds_nome
+                    .toLowerCase()
+                    .indexOf(ds_filtrar.toLowerCase()) >= 0;
+                }
+            );
+        }
+    },
+    methods: {
+        getListRegistros: function() {
+            fetch(
+                '/sistema/lista-sistemas',
+                {
+                    credentials: 'include'
+                }
+            )
+            .then(objResponse => objResponse.json())
+            .then(
+                arrJson => {
+                    this.arrSistemas = arrJson;
+                    this.arrSistemasOriginal = arrJson;
+                    this.sn_carregando = false;
+                }
+            );
+        },
+
+        goToEditar: function(nr_id) {
+            document.location = '/sistema/administrar/' + nr_id;
+        }
+    }
+});
