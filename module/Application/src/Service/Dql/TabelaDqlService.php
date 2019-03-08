8e27dccbd50f618e9dd259bdfd6e3e08b1a4da8c
Adriano Waltrick <adriano@unimestre.com>
2019-03-08 07:43:42 -0300

    Correcao nos JS para load local - e - correcao sql ordenacao fks

--- module/Application/src/Service/Dql/TabelaDqlService.php
+++ module/Application/src/Service/Dql/TabelaDqlService.php
@@ -23,9 +23,9 @@ class TabelaDqlService
                 t.ds_nome as ds_nome,
                 t_temp.id as id_temp,
                 t_temp.ds_nome as ds_nome_temp
-            from 
+            from
                 tabela t
-                                
+
                 inner join tabela t_temp ON (
                     t_temp.ds_nome = t.ds_nome
                     and t_temp.sn_temporario = 0
@@ -38,16 +38,21 @@ class TabelaDqlService
     public function listaTodasAsTabelas()
     {
         return '
-            select 
+            select
                 t.id as id,
-                t.ds_nome as ds_nome 
-            from 
+                t.ds_nome as ds_nome,
+                t_sistema.ds_nome as ds_nome_sistema,
+                concat(t_sistema.ds_nome, \' - \', t.ds_nome) as ds_nome_concat
+            from
                 \Application\Entity\Tabela t
-            where 
-                t.sn_excluido = 0 and 
+
+                INNER JOIN \Application\Entity\Sistema t_sistema
+            where
+                t.sn_excluido = 0 and
                 t.sn_temporario = 0
-            order by 
-              t.ds_nome
+            order by
+                t_sistema.ds_nome,
+                t.ds_nome
         ';
     }
 }
